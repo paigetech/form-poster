55f4aba2e3673e2108c11ab76360c088bb53e3c8
Paige Hubbell <paigetech@gmail.com>
2013-10-18 20:19:52 -0700

    form posting version one

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 var express = require('express');
-
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-app.listen(3000, 127.0.0.1);
-console.log("Express is listening!")
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.use(express.directory(__dirname + '/public/forms'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public/forms')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res){
+	res.write(app.directory)
+;});
+
+app.post('/form', function(req, res){
+	console.log(req.body);
+	res.send("Got the following data: " + JSON.stringify(req.body, null, 4));
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
